5de1e4bee575eff1f5e83dc28a5e746e45c6f87a
cuaderno init <eber.sanchez.c@gmail.com>
2026-06-17 21:49:48 -0400

    Agregar íconos SVG a los enlaces del menú de usuario para mejorar la visualización

--- App/Segment/Menu/User/menuUser.php
+++ App/Segment/Menu/User/menuUser.php
@@ -1,16 +1,16 @@
 <?php if ($connect ?? false == true) :?>
   <div class="flex-column top-end">
-    <a href="/panel/<?= $username ?? "Usuario";?>" class="textc">Configuración</a>
-    <a href="/salir" class="textc">Salir</a>
+    <a href="/panel/<?= $username ?? "Usuario";?>" class="textc">Configuración <?= svg("gear");?></a>
+    <a href="/salir" class="textc">Salir <?= svg("out");?></a>
   </div>
 <?php else :?>
   <?php if (!\Base\Module\Session::session_active()) :?>
     <div class="flex-column top-end">
-      <a href="/ingresar" class="textc">Ingresar</a>
+      <a href="/ingresar" class="textc">Ingresar <?= svg("user");?></a>
     </div>
   <?php else :?>
     <div class="flex-column top-end">
-      <a href="/" class="textc">Volver</a>
+      <a href="/" class="textc"><?= svg("arrow-l-l");?> Volver</a>
     </div>
   <?php endif?>
 <?php endif?>
