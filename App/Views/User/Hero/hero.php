f0de81eef692064c2821d663e4188acbbc79ca90
cuaderno init <eber.sanchez.c@gmail.com>
2026-06-19 21:56:44 -0400

    Reorganizar la estructura del componente Hero para mejorar la visualización del usuario

--- App/Views/User/Hero/hero.php
+++ App/Views/User/Hero/hero.php
@@ -1,11 +1,13 @@
 <main class="">
   <div class="textc hem15 flex-column center-center">
-
-    <div class="absolute top right p10 pr20">
-      
-    </div>
     
-    <h1>Hola, <?= $user ?? "Usuario";?></h1>
+    <div class="flex-column center-center hpx120">
+      <figure class="hpx100 wpx100 br100 back1">
+        <img src="<?= DIR_SHOW_MEDIA.'/Custom/eber10.png'?>" alt="" class="cover">
+      </figure>
+    </div>
+    <h1 class="x30">Hola, <?= $user ?? "Usuario";?></h1>
+    <p class="p40 pt10 pb10 text-c">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta eum ex, architecto atque molestiae delectus exercitationem perferendis asperiores quas voluptas ratione at sit, adipisci, sapiente perspiciatis eos autem deserunt ad?</p>
 
   </div>
 </main>
