f4328c5e43eda9227d225decce14b45009306963
cuaderno init <eber.sanchez.c@gmail.com>
2026-07-01 21:48:58 -0400

    Actualizar estilos en modalMenuLink: agregar clase 'texto' al enlace de perfil para mejorar la visualización.

--- App/Views/User/ModalMenuLink/modalMenuLink.php
+++ App/Views/User/ModalMenuLink/modalMenuLink.php
@@ -4,12 +4,12 @@
 <div class="flex-column center-center gap10 back-modal-item wrap">
 
   <div class="flex-column center-center gap15 wpx260 p30 br20 border-card-modal hover-scale-soft" style="background-color: #ebebeb;">
-hover-scale-soft    <figure class="ar-square wpx200 br15">
+    <figure class="ar-square wpx200 br15">
       <img src="<?= DIR_SHOW_MEDIA.$data["img"]?>" alt="" class="cover">
     </figure>
 
     <p class="x16 bold500"><?= $data["content"]?></p>
-    <a href="<?= $data["profile"]?>" class="x16"><?= $data["profile"]?></a>
+    <a href="<?= $data["profile"]?>" class="x16 texto"><?= $data["profile"]?></a>
   </div>
 
 </div>
