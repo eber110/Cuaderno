cc40dbdcabb1dcbfdd5fea330b6b370e724423a0
cuaderno init <eber.sanchez.c@gmail.com>
2026-07-12 01:11:47 -0400

    Agregar clase 'auto-submit' al formulario de cambio de fondo del perfil y ocultar botón de envío

--- App/Views/Dashboard/ContentPanel/contentPanel.php
+++ App/Views/Dashboard/ContentPanel/contentPanel.php
@@ -14,7 +14,7 @@
 
   <div id="content-remote-2" class="remote-content hidden">
     <p>Aquí va el formulario para cambiar el fondo del perfil</p>
-    <form action="/test/1" method="post">
+    <form class="auto-submit" action="/test/1" method="post">
       <div class="br50 wpx40 hpx40 flex-row center-center p0 overflow" style="background-color: <?= $card["backCard"][0]?>;">
         <input type="color" name="back_perfil" value="<?= $card["backCard"][0]?>" id="" class="wpx100 hpx100 p0 m0">
       </div>
@@ -23,7 +23,7 @@
         <option value="gradientDown" <?php if ($card["backCard"][1] == "gradientDown") echo "selected";?>>gradiente abajo</option>
         <option value="solid" <?php if ($card["backCard"][1] == "solid") echo "selected";?>>color solido</option>
       </select>
-      <input type="submit" value="guardar">
+      <input type="submit" value="guardar" class="hidden">
     </form>
   </div>
 
