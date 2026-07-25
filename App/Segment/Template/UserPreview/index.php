3ecef413dfcad4591121c6c4e403f19b923cb894
cuaderno init <eber.sanchez.c@gmail.com>
2026-07-25 19:20:21 -0400

    fix(styles): ajustar clase de borde en el perfil de usuario

--- App/Segment/Template/UserPreview/index.php
+++ App/Segment/Template/UserPreview/index.php
@@ -3,7 +3,7 @@
   _part("User.style.css");
 ?>
 <div class="wpx460 w-sml-100 h-dvh pt40 pb40 p-sml-0 flex-column center-center user-profile-preview">
-  <div class="flex-column between-center back-card shadow-card-preview color-text-card preview-profile p0 br30 br-mid-0 w100">
+  <div class="flex-column between-center back-card shadow-card-preview color-text-card preview-profile p0 br30 br-sml-0 w100">
     
     <header class="w100">
       <?php
