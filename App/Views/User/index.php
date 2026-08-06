fd21c7cf733d2e03e35f42de38bb3130f81aa273
cuaderno init <eber.sanchez.c@gmail.com>
2026-08-05 22:06:41 -0400

    feat(User): agregar overflow-y-scroll a la tarjeta de usuario en la vista

--- App/Views/User/index.php
+++ App/Views/User/index.php
@@ -4,7 +4,7 @@
 ?>
 <div class="container-xl container-xl-sml flex-column center-center text-protected back-card-container overflow-y-scroll">
   <div class="wpx580 w-sml-100 h-dvh pt40 p-sml-0">
-    <div class="flex-column between-center back-card color-text-card shadow-card dvh-cuaderno p0 brtl-desk-30 brtr-desk-30 brtl-mid-30 brtr-mid-30 brtl-sml-0 brtr-sml-0">
+    <div class="flex-column between-center back-card color-text-card shadow-card dvh-cuaderno p0 brtl-desk-30 brtr-desk-30 brtl-mid-30 brtr-mid-30 brtl-sml-0 brtr-sml-0 overflow-y-scroll">
       
       <header class="w100">
         <?php
