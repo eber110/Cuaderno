feb3fecf2e75be3387915817fce09c385b2d42fd
cuaderno init <eber.sanchez.c@gmail.com>
2026-08-24 18:48:59 -0400

    feat: actualizar clases de opacidad y tamaños de fuente para mejorar la presentación

--- App/Views/Home/Description/description.php
+++ App/Views/Home/Description/description.php
@@ -1,15 +1,15 @@
 <div class="container-xl back8 observer">
-  <div class="container container-xl-mid container-sml flex-column gap20 flex-row center-center flex-column-sml center-center-sml pt150 pb150">
-    <div class="w80 w-sml-100 back2 br15 flex-column center-center m-mid-15 ob-50 dl-200 slide-in-left animate-slow">
+  <div class="container container-xl-mid container-sml flex-column gap20 flex-row center-center flex-column-sml center-center-sml pt150 pb150 h-dvh-sml">
+    <div class="w80 w-sml-100 back2 br15 flex-column center-center m-mid-15 ob-40 dl-200 slide-in-left animate-slow">
       <img src="<?= DIR_UPLOAD_MEDIA_STATIC."section2.webp"?>" alt="" class="cover ar-square">
     </div>
     <div class="w100 flex-column around-start flex-column-sml top-start-sml gap20 p15 p-sml-0">
       <div class="flex-column gap10">
-        <p class="oswald bold600 x28 color4 slide-in-right ob-50 dl-100 animate-slow">CÓDIGO ULTRALIGERO</p>
-        <h2 class="oswald bold700 capitalize-p line-h10 x80 x-mid-50 x-sml-40 textc slide-in-right ob-50 dl-200 animate-slow m0 p0">Todo lo que necesitas. Nada que sobre.</h2>
-        <p class="bold500 x20 x-sml-18 slide-in-right ob-50 dl-400 animate-slow pt20 pb20 pt-mid-10 pb-mid-20 pt-sml-5 textc">Eliminamos el peso innecesario para entregarte una plataforma limpia, moderna y enfocada exclusivamente en hacer crecer tus conversiones.</p>
+        <p class="oswald bold600 x28 color4 slide-in-right ob-40 dl-100 animate-slow">CÓDIGO ULTRALIGERO</p>
+        <h2 class="oswald bold700 capitalize-p line-h10 x80 x-mid-60 x-sml-40 textc slide-in-right ob-40 dl-200 animate-slow m0 p0">Todo lo que necesitas. Nada que sobre.</h2>
+        <p class="bold500 x20 x-sml-18 slide-in-right ob-40 dl-400 animate-slow pt20 pb20 pt-mid-10 pb-mid-20 pt-sml-5 textc">Eliminamos el peso innecesario para entregarte una plataforma limpia, moderna y enfocada exclusivamente en hacer crecer tus conversiones.</p>
       </div>
-      <a href="/registrar" class="slide-in-right ob-50 dl-600 p10 pr30 pl30 br50 w-auto back4-hover texto x20 x-sml-18 bold600 pointer">
+      <a href="/registrar" class="slide-in-right ob-40 dl-600 p10 pr30 pl30 br50 w-auto back4-hover texto x20 x-sml-18 bold600 pointer">
         <p>Crear mi hub</p>
       </a>
     </div>
