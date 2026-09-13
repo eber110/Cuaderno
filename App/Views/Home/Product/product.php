576db174002f636b5e619f6c16b73f7758088ba9
cuaderno init <eber.sanchez.c@gmail.com>
2026-09-13 20:59:16 -0300

    feat: agregar clase de desbordamiento a contenedor y mejorar la estructura de imágenes en la vista del producto

--- App/Views/Home/Product/product.php
+++ App/Views/Home/Product/product.php
@@ -1,4 +1,4 @@
-<div class="container-xl back5 observer">
+<div class="container-xl back5 observer overflow">
   <div class="container container-xl-mid container-sml flex-column gap20 flex-row center-center flex-column-sml center-center-sml pt100 pb100">
     <div class="w100 flex-column around-start flex-column-sml top-start-sml gap20 p15 p-sml-0">
       <div>
@@ -9,8 +9,17 @@
         <p>Inicia gratis</p>
       </a>
     </div>  
-    <div class="w100 w-sml-100 back2 br15 flex-column center-center m-mid-15">
-      <img src="<?= DIR_UPLOAD_MEDIA_STATIC."prod.webp"?>" alt="" class="cover ar-square">
+    <div class="w100 w-sml-100 flex-column center-center m-mid-15">
+      <figure class="br30 w60 w-mid-80">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."prodBack.webp"?>" alt="" class="contain">
+      </figure>
+      <div class="flex-column center-center gap30 absolute w50 w-mid-80" style="rotate: 5deg; filter: drop-shadow(0px 0px 5px #0000002d);">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."linkCamp.webp"?>" alt="" class="hover-scale-soft slide-in-bottom ob-50 dl-1000 animate-slow">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."link.webp"?>" alt="" class="hover-scale-soft slide-in-bottom ob-50 dl-800 animate-slow">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."linkProd.webp"?>" alt="" class="hover-scale-soft slide-in-bottom ob-50 dl-600 animate-slow">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."linkBanner.webp"?>" alt="" class="hover-scale-soft slide-in-bottom ob-50 dl-400 animate-slow">
+        <img src="<?= DIR_UPLOAD_MEDIA_STATIC."linkCamp2.webp"?>" alt="" class="hover-scale-soft slide-in-bottom ob-50 dl-200 animate-slow">
+      </div>
     </div>
   </div>
 </div>
